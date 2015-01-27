1691bf334a706d316320789bd025e355cd158570
Bruno Fonseca <bfcomput@gmail.com>
2015-01-26 22:04:56 -0200

    Set Author Plugin and Rename correctly the plugin

--- init.rb
+++ init.rb
@@ -4,10 +4,10 @@ require 'issue_templates_notes_hook'
 
 Redmine::Plugin.register :redmine_issue_templates_notes do
   name 'Redmine Issue Templates Notes plugin'
-  author 'Bruno Fonseca & Adriano Cinto Ceccareli'
+  author 'Bruno Fonseca'
   description 'Plugin para template de notas'
-  author_url 'http://www.sisloc.com.br/institucional/equipe'
-  version '0.0.1'
+  author_url 'https://github.com/brunofonseca'
+  version '1.0.0'
   settings :default => {'empty' => true}, :partial => 'settings/redmine_notes'
   
   menu :admin_menu, :redmine_issue_templates_notes, {:controller => 'issue_templates_notes_settings',:action => 'back_partial_init'}, :caption => :template_notes, :after => :settings, :if => Proc.new{User.current.admin?} 
