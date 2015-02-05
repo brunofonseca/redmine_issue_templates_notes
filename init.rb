55be0d00ac05a5ef1c28f6c158a7a3de32620989
Bruno Fonseca <bfcomput@gmail.com>
2015-02-04 22:15:21 -0200

    Incremented a new layout for presentation plugin in view and actualized jQuery attributes.

--- init.rb
+++ init.rb
@@ -5,9 +5,9 @@ require 'issue_templates_notes_hook'
 Redmine::Plugin.register :redmine_issue_templates_notes do
   name 'Redmine Issue Templates Notes plugin'
   author 'Bruno Fonseca'
-  description 'Plugin para template de notas'
+  description 'A Redmine Plugin for manager template notes in your project'
   author_url 'https://github.com/brunofonseca'
-  version '1.0.0'
+  version '1.0.1'
   settings :default => {'empty' => true}, :partial => 'settings/redmine_notes'
   
   menu :admin_menu, :redmine_issue_templates_notes, {:controller => 'issue_templates_notes_settings',:action => 'back_partial_init'}, :caption => :template_notes, :after => :settings, :if => Proc.new{User.current.admin?} 
