448a006e8356e6430038989e506a2eaee2229bf9
Bruno Fonseca <bruno-fonseca@outlook.com>
2017-12-28 13:41:07 -0200

    Fix #4 Fix #6

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,31 @@
-Rails.application.routes.draw do  
+Rails.application.routes.draw do
+
   resources :issue_templates_notes
   resources :issue_templates_notes_settings
-  match 'settings/plugin/redmine_issue_templates_notes', :to => 'issue_templates_notes_settings#back_partial_init', :via => [:get,:post]
-  match 'issue_templates_notes/:id/edit', :to => 'issue_templates_notes#edit'
-  match 'issue_templates_notes/preview', :to => 'issue_templates_notes#preview', :via => [:get, :post]
-  match 'issue_templates_notes/:id/template_send', :to => 'issue_templates_notes#template_send', :via => [:get, :post]
-  match 'settings/plugin/redmine_issue_templates_notes/users_authorization', :to => 'issue_templates_notes_settings#user_auth', :via => [:get,:post]
+
+  match 'settings/plugin/redmine_issue_templates_notes',
+        :controller  => 'issue_templates_notes_settings',
+        :action => 'back_partial_init',
+        :via => :get
+
+  match 'issue_templates_notes/:id/edit',
+        :controller => 'issue_templates_notes',
+        :action => :edit,
+        :via => :get
+
+  match 'issue_templates_notes/preview',
+        :controller => 'issue_templates_notes',
+        :action => 'preview',
+        :via => [:get,:post]
+
+  match 'issue_templates_notes/:id/template_send',
+        :controller => 'issue_templates_notes',
+        :action => 'template_send',
+        :via => [:get,:post]
+
+  match 'settings/plugin/redmine_issue_templates_notes/users_authorization',
+        :controller => 'issue_templates_notes_settings',
+        :action => 'user_auth',
+        :via => [:get,:post]
+
 end
